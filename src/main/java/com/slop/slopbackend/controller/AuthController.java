e3507f4bafe808aa39e42d007978b8ad5fbe4f1d
jaideep.c <jaideep.c@daytonatec.com>
2022-09-16 01:24:53 +0530

    Cross Origin

--- src/main/java/com/slop/slopbackend/controller/AuthController.java
+++ src/main/java/com/slop/slopbackend/controller/AuthController.java
@@ -14,15 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("auth")
+@CrossOrigin("*")
 public class AuthController {
     private final UserDetailsServiceImpl userDetailsService;
     private final UserService userService;
