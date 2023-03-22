977228087732f6e8ba96464f2ad3570b786166ed
jaideep-c <jaideepcvl@gmail.com>
2023-03-22 15:53:49 +0530

    Changes random OTP generator

--- src/main/java/com/slop/slopbackend/utility/RandomKeyGenerator.java
+++ src/main/java/com/slop/slopbackend/utility/RandomKeyGenerator.java
@@ -6,12 +6,18 @@ import java.util.Base64;
 public class RandomKeyGenerator {
 
     private static final int KEY_LENGTH = 20; // Key length in bytes
-
+    private static final SecureRandom random = new SecureRandom();
     public static String generateRandomKey() {
-        SecureRandom random = new SecureRandom();
         byte[] keyBytes = new byte[KEY_LENGTH];
         random.nextBytes(keyBytes);
         return Base64.getUrlEncoder().withoutPadding().encodeToString(keyBytes);
+    }
+    public static String generateRandomString(int len){
+        StringBuilder sb = new StringBuilder(len);
+        for(int i = 0; i < len; i++) {
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
     }
 }
 
